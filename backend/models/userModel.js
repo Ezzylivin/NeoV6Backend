0235384b4504eb4d61696c43b657fb16f2150a3c
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-02 20:32:48 -0400

    Create userModel.js

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+// File: backend/models/userModel.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  exchangeApiKey: { type: String }, // Will store ENCRYPTED key
+  exchangeApiSecret: { type: String }, // Will store ENCRYPTED secret
+});
+
+// Hash password before saving a new user
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare entered password with the hashed one in the DB
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
+

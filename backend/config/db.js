69ab22a49a16d0fa68178fc20fcc10a45ae64ba5
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-03 15:17:20 -0400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DATABASE_URL,);
     console.log('MongoDB Connected');
   } catch (err) {
     console.error(`MongoDB Connection Error: ${err.message}`);

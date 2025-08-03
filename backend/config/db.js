4907e835810ca694f2c70e87d655b75ef34d8ef9
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-02 20:33:58 -0400

    Create db.js

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+// File: backend/config/db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log('✅ MongoDB Connected');
+  } catch (err) {
+    console.error(`❌ MongoDB Connection Error: ${err.message}`);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

71fa05a494f533f55ab071eba2a5012352801af4
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-04 13:21:22 -0400

    Update backtestModel.js

--- backend/models/backtestModel.js
+++ backend/models/backtestModel.js
@@ -2,14 +2,14 @@
 import mongoose from 'mongoose';
 
 const backtestSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   timeframe: { type: String, required: true },
-  initialBalance: Number,
-  finalBalance: Number,
-  profit: Number,
-  totalTrades: Number,
-  candlesTested: Number,
-  createdAt: { type: Date, default: Date.now }
+  initialBalance: { type: Number, required: true },
+  finalBalance: { type: Number, required: true },
+  profit: { type: Number, required: true },
+  totalTrades: { type: Number, required: true },
+  candlesTested: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model('Backtest', backtestSchema);

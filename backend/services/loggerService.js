3cff619a3c34e62839c0660d7ae5a251f42c6329
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-04 12:47:53 -0400

    Update loggerService.js

--- backend/services/loggerService.js
+++ backend/services/loggerService.js
@@ -1,11 +1,10 @@
-const Log = require('../models/logModel');
+// File: backend/services/loggerService.js
+import Log from '../models/logModel.js';
 
-const logToDb = async (userId, message) => {
+export const logToDb = async (userId, message) => {
   try {
     await Log.create({ userId, message });
   } catch (err) {
     console.error(`[Logger Error]: ${err.message}`);
   }
 };
-
-module.exports = { logToDb };

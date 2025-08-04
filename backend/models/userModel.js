e64772cf1512320ef44af7e9929e88db9d97ea1f
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-04 09:15:37 -0400

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,23 @@
-// File: backend/models/userModel.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  exchangeApiKey: { type: String }, // Will store ENCRYPTED key
-  exchangeApiSecret: { type: String }, // Will store ENCRYPTED secret
-});
+  apiKey: String,
+  apiSecret: String,
+}, { timestamps: true });
 
-// Hash password before saving a new user
+// Password hash before save
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-// Method to compare entered password with the hashed one in the DB
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+// Password compare method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
-

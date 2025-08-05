a5217856273bea90f81e5bd18aef77978cd5a7c8
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-05 19:08:37 -0400

    Update and rename authRoutes.js to authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -15,11 +15,11 @@ router.post('/register', async (req, res) => {
   const { email, password, role } = req.body;
   if (!email || !password) return res.status(400).json({ message: 'Please enter all fields' });
 
-  const userExists = await User.findOne({ email });
+  const userExists = await user.findOne({ email });
   if (userExists) return res.status(400).json({ message: 'User already exists' });
 
   try {
-    const user = await User.create({ email, password });
+    const user = await user.create({ email, password });
     res.status(201).json({ access_token: generateToken(user._id) });
   } catch (error) {
     res.status(500).json({ message: 'Server error' });

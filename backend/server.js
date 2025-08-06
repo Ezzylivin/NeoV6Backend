3762aa33ff7bd6adc324d5f0637eccddf85d2378
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-06 00:04:01 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,8 @@ app.get('/', (req, res) => {
 
 // VVVV THIS IS WHERE YOU "PLUG IN" YOUR IMPORTED ROUTER VVVV
 // Any request that starts with '/api' will be handled by the 'apiRoutes' router.
-app.use('/api', apiRoutes);
+
+ //app.use('/api', apiRoutes);
 
 
 // --- Error Handling Middleware (Optional but Recommended) ---

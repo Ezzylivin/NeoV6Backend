1e6be0583ef5ef9ab24e0835eddcb2ac87f8ea97
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-07 14:59:21 -0400

    Update user.js

--- backend/dbStructure/user.js
+++ backend/dbStructure/user.js
@@ -2,18 +2,19 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
-const exchangeKeySchema = new mongoose.Schema({
-  exchange: { type: String, required: true }, // e.g., 'binance', 'coinbase'
-  apiKey: { type: String, required: true },
-  apiSecret: { type: String, required: true },
-}, { _id: false });
-
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: [true, 'Email Required'] },
   password: { type: String, required: [true, 'Password is required'], minlength: 8 },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
   exchangeKeys: [exchangeKeySchema],
 }, { timestamps: true });
+
+const exchangeKeySchema = new mongoose.Schema({
+  exchange: { type: String, required: true }, // e.g., 'binance', 'coinbase'
+  apiKey: { type: String, required: true },
+  apiSecret: { type: String, required: true },
+}, { _id: false });
+
 
 // Hash password before save
 userSchema.pre('save', async function (next) {
@@ -27,5 +28,5 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
-export default User;
+const user = mongoose.model('User', userSchema);
+export default user;

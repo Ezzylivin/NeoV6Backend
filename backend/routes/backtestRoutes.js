c4b513cb3c841d660f56036503ec947d670827fb
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-07 21:52:56 -0400

    Update backtestRoutes.js

--- backend/routes/backtestRoutes.js
+++ backend/routes/backtestRoutes.js
@@ -1,15 +1,11 @@
 // File: backend/routes/backtestRoutes.js
 import express from 'express';
 // import { protect } from '../middleware/authMiddleware.js';
-import { createBacktest, getBacktestByUser } from '../controllers/backtestController.js';
+import { runAndSaveBacktests  } from '../controllers/backtestController.js';
 
 const router = express.Router();
 
 // GET: Fetch stored backtest results
-router.post('/create', createBacktest);
+router.post('/api/backtests', runAndSaveBacktests);
 
-// POST: Run a new backtest and store the result
-router.get['/use/:userId', getBacktesByUser);
-
-
-export 
+export default router;

cce9c664e93d6fa343b7f689b85fda653a55bfbf
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-10 14:36:11 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 // --- Middleware Configuration ---
 
 // 1. Define the dynamic CORS options object.
-const corsOptions = {
+app.options('*', const corsOptions = {
   /**
    * The origin property is a function that determines if a request's
    * origin is allowed.

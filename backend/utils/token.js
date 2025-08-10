d0a85eaea70243725f8039f549db118b98bf721d
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-10 10:07:53 -0400

    Update token.js

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,9 +1,9 @@
-// File: /backend/utils/token.js
+// File: backend/utils/token.js
 import jwt from 'jsonwebtoken';
 
 export const token = (user) => {
   return jwt.sign(
-    { id: user._id },
+    { id: user._id, username: user.username },
     process.env.JWT_SECRET,
     { expiresIn: '30d' }
   );

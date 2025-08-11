64e98dc3510547d3f2e1f8447af59a9a4d3bfbbf
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-11 15:20:23 -0400

    Update loginService.js

--- backend/services/loginService.js
+++ backend/services/loginService.js
@@ -2,15 +2,17 @@
 import {token} from '../utils/token.js';
 import User from '../dbStructure/user.js';
 
-export const loginUser = async (email, password) => {
+export const loginUser = async (username, password) => {
   const user = await User.findOne({ email });
 
   if (!user || !(await user.comparePassword(password))) {
     throw new Error('Invalid email or password');
   }
 
+   const jwtToken = generateToken(newUser._id);
+
   return {
-    token: token(user._id),
+    token: jwtToken,
     user: {
       id: user._id,
       username: user.username,

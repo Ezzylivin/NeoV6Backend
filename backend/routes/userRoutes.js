3fdd58eefa5ce120af719a8b6f3d60a120afdb8e
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-11 20:10:40 -0400

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@
 import express from 'express';
 // Import the CONTROLLER functions, not the services
 import { registerUser, loginUser } from '../controllers/userController.js';
+import { verifyToken } from '../middleware/authMiddleware.js
 
 const router = express.Router();
 

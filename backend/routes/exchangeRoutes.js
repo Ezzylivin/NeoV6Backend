c0bc1e4998e89e28b09f179f848c5d89d707ec26
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-12 14:54:06 -0400

    Update exchangeRoutes.js

--- backend/routes/exchangeRoutes.js
+++ backend/routes/exchangeRoutes.js
@@ -1,14 +1,14 @@
 // File: backend/routes/exchangeRoutes.js
 import express from 'express';
-import { verifyToken } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
 import { saveExchangeKey, getExchangeKeys } from '../controllers/exchangeController.js';
 
 const router = express.Router();
 
 // Save user's exchange keys
-router.post('/keys',  verifyToken, saveExchangeKey);
+router.post('/keys',  protect, saveExchangeKey);
 
 // Get user's exchange keys
-router.get('/keys',  verifyToken, getExchangeKeys);
+router.get('/keys',  protect, getExchangeKeys);
 
 export default router;

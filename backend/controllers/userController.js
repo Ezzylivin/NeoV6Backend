66ff6e49c215f1ea4811d8be53bc91580a051e4f
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-13 01:25:56 -0400

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,7 +19,7 @@ export const loginUser = async (req, res) => {
   const { email , password } = req.body;
   try {
     // It delegates EVERYTHING to the service. No 'User.findOne' here.
-    const result = await userService.loginUser(email, password);
+    const result = await userService.loginUser({email, password});
     res.status(200).json(result);
   } catch (error) {
     res.status(401).json({ message: error.message });

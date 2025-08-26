5137ef631de8cbce4befc693ec6ae27f1d2c51c2
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-26 12:21:38 -0400

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,13 +8,12 @@ export const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const user = await userService.registerUser(username, email, password);
-  res.status(201).json({
-  _id: user.id,
-  username: user.username,
-  email: user.email,
-  token: generateToken(user.id),
-});
-
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user.id),
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -31,4 +30,13 @@ export const loginUser = async (req, res) => {
       email: user.email,
       token: generateToken(user.id),
     });
-  } catch
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
+// Controller for Get Me
+export const getMe = (req, res) => {
+  // Relies on authentication middleware
+  res.status(200).json(req.user);
+};

91ccc51e642c2959dc7e5d62d8e307732a8663ca
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-27 22:26:25 -0400

    Update logRoutes.js

--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -5,9 +5,9 @@ import { getLogs, createLog } from '../controllers/logController.js';
 const router = express.Router();
 
 // GET logs — Admin sees all, user sees only their own
-router.get('/logs/get', getLogs);
+router.get('/get', getLogs);
 
 // POST a new log
-router.post('/logs/create', createLog);
+router.post('/create', createLog);
 
 export default router;
